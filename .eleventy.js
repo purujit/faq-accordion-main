91e9c502adb0fa0b57da139aef29178b62c75684
Steve McBride <steve@mcbride.com>
2024-01-31 23:13:47 -0800

    Done

---
+++ .eleventy.js
@@ -0,0 +1,6 @@
+const pluginWebc = require('@11ty/eleventy-plugin-webc');
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPlugin(pluginWebc)
+    eleventyConfig.addPassthroughCopy('assets');
+    eleventyConfig.addPassthroughCopy('*.css');
+}
